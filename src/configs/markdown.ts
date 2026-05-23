1d96e7aedd74d1435a2e25c9e49ca0d3b0de6443
suressk <suresskun@outlook.com>
2026-05-23 23:55:31 +0800

    fix: update .md configs

--- src/configs/markdown.ts
+++ src/configs/markdown.ts
@@ -1,6 +1,7 @@
 import type { OptionsFiles, OptionsOverrides, TypedFlatConfigItem } from '../types'
 
-import { GLOB_MARKDOWN } from '../globs'
+import { GLOB_MARKDOWN, GLOB_MARKDOWN_CODE } from '../globs'
+import { tryInteropDefault } from '../utils'
 
 export async function markdown(
   options: OptionsOverrides & OptionsFiles & { componentExts?: string[] } = {},
@@ -8,12 +9,27 @@ export async function markdown(
   const { componentExts: _componentExts = [], overrides = {} } = options
   const files = options.files ?? [GLOB_MARKDOWN]
 
+  const pluginMarkdown = await tryInteropDefault(import('@eslint/markdown'))
+
   return [
     {
+      name: 'suressk/markdown/plugin',
+      plugins: {
+        markdown: pluginMarkdown,
+      },
+    },
+    {
       files,
-      name: 'suressk/markdown/rules',
+      name: 'suressk/markdown/processor',
+      processor: 'markdown/markdown',
       rules: {
         'no-irregular-whitespace': 'off',
+      },
+    },
+    {
+      files: [GLOB_MARKDOWN_CODE],
+      name: 'suressk/markdown/rules',
+      rules: {
         'no-undef': 'off',
         'no-unused-vars': 'off',
         'unused-imports/no-unused-vars': 'off',
